db4bf187fa5fb6f8fc32ecbde179ec05c190e161
Yatin Gupta <guptayatin4@gmail.com>
2023-07-08 15:20:49 -0500

    The cam can now detect the eye

--- main.py
+++ main.py
@@ -2,17 +2,18 @@ import cv2
 import mediapipe as mp
 
 cam = cv2.VideoCapture(0)
-face = mp.solutions.face_mesh.FaceMesh()
+face = mp.solutions.face_mesh.FaceMesh(refine_landmarks = True)
 
 while True:
     _, frame = cam.read()
+    frame = cv2.flip(frame, 1)
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     output = face.process(rgb_frame)
     landmark_points = output.multi_face_landmarks
     frame_height, frame_width, _ = frame.shape
     if landmark_points:
         landmarks = landmark_points[0].landmark
-        for landmark in landmarks:
+        for landmark in landmarks[474:478]:
             x = int(landmark.x * frame_width)
             y = int(landmark.y * frame_height)
             cv2.circle(frame, (x, y), 3, (0, 255, 0))
