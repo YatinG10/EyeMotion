aa1dd414002512edab2de364a2923db8d52c8b18
Yatin Gupta <guptayatin4@gmail.com>
2023-07-08 15:16:26 -0500

    Show the landmarks on the face in the cam

--- main.py
+++ main.py
@@ -9,8 +9,14 @@ while True:
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     output = face.process(rgb_frame)
     landmark_points = output.multi_face_landmarks
-    print(landmark_points)
-    
+    frame_height, frame_width, _ = frame.shape
+    if landmark_points:
+        landmarks = landmark_points[0].landmark
+        for landmark in landmarks:
+            x = int(landmark.x * frame_width)
+            y = int(landmark.y * frame_height)
+            cv2.circle(frame, (x, y), 3, (0, 255, 0))
+
     cv2.imshow('Eye Controlled Mouse', frame)
     cv2.waitKey(1)
 
